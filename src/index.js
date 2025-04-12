c61892ed8227608e72dd0f5c9b95d9adc650c60f
Matthew <matthew@newedgeengineering.com>
2025-04-12 12:23:13 +0100

    Add commander package for Command Line Interface

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-// Parse the CSV log file.
-// Identify each job or task and track its start and finish times.
-// Calculate the duration of each job from the time it started to the time it finished.
-// Analyse data.
+#!/usr/bin/env node
+
+'use strict';
+
+const { Command } = require('commander');
+
+const pkg = require('../package.json');
+
+const program = new Command();
+
+program.version(pkg.version);
+program
+  .command('analyse')
+  .argument('<filepath>', 'The path to file to be analyse.')
+  .description('')
+  .action(function (filepath) {
+    // Parse the CSV log file.
+    // Identify each job or task and track its start and finish times.
+    // Calculate the duration of each job from the time it started to the time it finished.
+    // Analyse data.
+    console.debug("hello world")
+  });
+program.parse(process.argv);
